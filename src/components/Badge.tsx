0918c1146ff85002a966c2e0181aef988c567333
nich2198 <128601463+nich2198@users.noreply.github.com>
2024-03-22 13:30:15 +0100

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Dialog Intelligens</span>
+        <span> Dialog Intelligen</span>
       </a>
     </span>
   )
